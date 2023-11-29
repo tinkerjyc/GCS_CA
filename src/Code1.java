d77f8e4de7c9afaa9ab63f7f3d12a483421675da
jyc <tinkerjyc@gmail.com>
2023-11-29 00:02:34 -0500

    Create Code1 structure.

--- src/Code1.java
+++ src/Code1.java
@@ -1,7 +1,55 @@
+/**
+ * Yicheng Jiang Coding Activity 1 - Making API Calls
+ **/
 public class Code1 {
+  //Create a static variable favour to store the favourite cities.
+  public static String[] favour;
+  //Create the limits for update favourite cities.
+  public static int LIMIT = 3;
+
+  /**
+   * Drive code with the user interface and interact with their favourite.
+   *
+   * @param args user inputs
+   */
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+
   }
 
- 
+  /**
+   * Check function to print the current weather details of the city.
+   *
+   * @param city the name of the city user want to check
+   */
+  public static void check(String city) {
+
+  }
+
+  /**
+   * Adding the city to user's favourite list with limits of 3.
+   *
+   * @param city   the array of city names
+   * @param favour the favourite city list
+   */
+  public static void addCity(String[] city, String[] favour) {
+
+  }
+
+  /**
+   * Print the favourite city list.
+   */
+  public static void listFavouriteCities() {
+  }
+
+
+  /**
+   * Deleting the city to user's favourite list with limits of 3.
+   *
+   * @param city   the array of city names
+   * @param favour the favourite city list
+   */
+  public static void deleteCity(String[] city, String[] favour) {
+
+  }
 }
