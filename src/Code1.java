4581843a521712b2aaa1139e7c391909b54b9ef7
jyc <tinkerjyc@gmail.com>
2023-11-29 19:44:39 -0500

    Finished Code1.

--- src/Code1.java
+++ src/Code1.java
@@ -2,10 +2,14 @@
  * Yicheng Jiang Coding Activity 1 - Making API Calls
  */
 
-import java.util.Arrays;
-import java.util.Locale;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Code1 {
   //Create a static variable favour to store the favourite cities.
@@ -130,11 +134,30 @@ public class Code1 {
     String urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + LOCATION +
             "&appid=" + API_KEY;
 
+    try {
+      StringBuilder result = new StringBuilder();
+      URL url = new URL(urlString);
+      URLConnection urlCon = url.openConnection();
+      BufferedReader rd = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
+      String line;
+      while ((line = rd.readLine()) != null) {
+        result.append(line);
+      }
+      rd.close();
+      System.out.println(result);
+
+    } catch (MalformedURLException e) {
+      System.out.println("Invalidate city name!");
+      e.printStackTrace();
+    } catch (IOException e) {
+      System.out.println("Invalidate city name!");
+      e.printStackTrace();
+    }
 
   }
 
   /**
-   * Adding the city to user's favourite list with limits of 3.
+   * Adding the cities to user's favourite list with limits of 3.
    *
    * @param cities the array of city names
    */
@@ -146,16 +169,19 @@ public class Code1 {
   }
 
   /**
-   * Print the favourite city list.
+   * Print the favourite city list and their current weather.
    */
   public static void listFavouriteCities() {
     System.out.println("Your favourite cites are: ");
-    System.out.print(favour);
+    for(String tmp:favour){
+      System.out.println(tmp);
+      check(tmp);
+    }
   }
 
 
   /**
-   * Deleting the city to user's favourite list with limits of 3.
+   * Updating the cities to user's favourite list with limits of 3.
    *
    * @param cities the array of city names
    */
